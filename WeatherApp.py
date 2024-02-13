0de9fab77d7af5288639d797b2d80e2b9c27683c
Emad Rouf <69056642+Emad-R@users.noreply.github.com>
2024-02-13 12:51:02 -0500

    Add files via upload

---
+++ WeatherApp.py
@@ -0,0 +1,28 @@
+import requests
+import pytemperature
+api_key = 'Provide_API_Key'
+
+nameOfCity = input("Provide the name of the city: ")
+
+result = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={nameOfCity}&appid={api_key}')
+
+if result.status_code == 200:
+    data = result.json()
+    kelvin = data['main']['temp']
+    kelvin = round(kelvin,2)
+    desc = data['weather'][0]['description']
+    celsius = round(pytemperature.k2c(kelvin),2)
+    fahrenheit = round(pytemperature.k2f(kelvin),2)
+    humidity = data['main']['humidity']
+    pressure = data['main']['pressure']
+    wind = data['wind']['speed']
+    
+    print(f'Humidity: {humidity}%')
+    print(f'Wind: {round(wind*2.237,2)} mph')
+    print(f'Temperature in Kelvin: {kelvin} K')
+    print(f'Temperature in Celsius: {celsius} C')
+    print(f'Temperature in Fahrenheit: {fahrenheit} F')
+    print(f'Description: {desc.capitalize()}')
+
+else:
+    print("There was an error receiving the data, please retry.")
